f238df9e203dfb601bfff7e3b834d1373a2808ad
vanesh <vanesh419@gmail.com>
2021-10-06 18:07:40 +0530

    added intial commit

---
+++ src/main/java/com/democomapny/first_vdemomaven_project/App.java
@@ -0,0 +1,13 @@
+package com.democomapny.first_vdemomaven_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
